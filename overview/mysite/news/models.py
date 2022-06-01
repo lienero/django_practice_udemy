8c67fbc6d6591d842374f35e272e20606abc7e0c
이경민 <lienero@naver.com>
2022-06-01 16:16:11 +0900

    디장고 모델 작성

--- overview/mysite/news/models.py
+++ overview/mysite/news/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Reporter(models.Model):
+    full_name = models.CharField(max_length=70)
+
+    def __str__(self):
+        return self.full_name
+
+
+class Article(models.Model):
+    pud_date = models.DateField()
+    headline = models.CharField(max_length=200)
+    content = models.TextField
+    Reporter = models.ForeignKey(Reporter, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.headline
