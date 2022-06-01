a3eeb42242fd00940e914f4e0478bf55460ff8d6
이경민 <lienero@naver.com>
2022-06-01 17:42:31 +0900

    데모 URL 설정, 템플릿 작성

--- overview/mysite/news/views.py
+++ overview/mysite/news/views.py
@@ -1,3 +1,10 @@
+from multiprocessing import context
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Article
+
+
+def year_archive(request, year):
+    a_list = Article.objects.filter(pub_date__year=year)
+    context = {'year': year, 'articel_list': a_list}
+    return render(request, 'news/year_archibe.html', context)
