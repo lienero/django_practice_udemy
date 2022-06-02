b75f07667dfb9b06d15be11128fc5ba7994f14dd
이경민 <lienero@naver.com>
2022-06-02 18:55:09 +0900

    step 2 투표기능 작성

--- tutorial/mysite/polls/views.py
+++ tutorial/mysite/polls/views.py
@@ -1,7 +1,10 @@
+from django.http import HttpResponse, HttpResponseRedirect
 from django.http import HttpResponse
+from django.urls import reverse
+
 
 from django.shortcuts import get_object_or_404, render
-from .models import Question
+from .models import Question, Choice
 
 
 def index(request):
@@ -14,4 +17,22 @@ def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
 
-# Create your views here.
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
