1b19ea5b6fe65971714fd00e92d98f168d0c2e4a
이경민 <lienero@naver.com>
2022-06-03 15:36:36 +0900

    관리자페이지에 선택지 추가

--- tutorial/mysite/polls/admin.py
+++ tutorial/mysite/polls/admin.py
@@ -3,12 +3,18 @@ from django.contrib import admin
 from .models import Choice, Question
 
 
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None, {'fields': ['question_text']}),
         # {'fields': ['pub_date']} 에 복수의 필드를 설정하면 그룹핑이 된다.
         ('Date information', {'fields': ['pub_date']}),
     ]
+    inlines = [ChoiceInline]
 
 
 admin.site.register(Question, QuestionAdmin)
