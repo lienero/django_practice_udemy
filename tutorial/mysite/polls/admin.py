b920a3012f9df899be8d2dfd474f326628f161dd
이경민 <lienero@naver.com>
2022-06-03 15:29:47 +0900

    관리자 페이지 변경

--- tutorial/mysite/polls/admin.py
+++ tutorial/mysite/polls/admin.py
@@ -2,7 +2,14 @@ from django.contrib import admin
 
 from .models import Choice, Question
 
-admin.site.register(Question)
-admin.site.register(Choice)
 
-# Register your models here.
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        # {'fields': ['pub_date']} 에 복수의 필드를 설정하면 그룹핑이 된다.
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
