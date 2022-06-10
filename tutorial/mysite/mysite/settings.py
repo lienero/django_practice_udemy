99628549a41f02c1285992d054ac8b1865582828
이경민 <lienero@naver.com>
2022-06-10 15:35:28 +0900

    모델 폼 테스트용 프로젝트 생성

--- tutorial/mysite/mysite/settings.py
+++ tutorial/mysite/mysite/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'lect_030103.apps.Lect030103Config',
+    'model_form_test.apps.ModelFormTestConfig',
     'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
