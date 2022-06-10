f842d1719cf6b4c46c49dcf77b52e1c8f0b79c23
이경민 <lienero@naver.com>
2022-06-10 15:45:20 +0900

    뷰설정

--- tutorial/mysite/model_form_test/views.py
+++ tutorial/mysite/model_form_test/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .forms import AuthorForm, BookForm
+
+
+def author(request):
+    form = AuthorForm()
+    return render(request, 'form_test/author.html', {'from': form})
+
+
+def book(request):
+    form = BookForm()
+    return render(request, 'from_test/book.html', {'from': form})
 
 # Create your views here.
