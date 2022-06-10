b93e481276f668fa3ef6b42c2484dcb725661ecf
이경민 <lienero@naver.com>
2022-06-10 15:39:58 +0900

    폼 파일 설정

---
+++ tutorial/mysite/model_form_test/forms.py
@@ -0,0 +1,16 @@
+from dataclasses import field
+from pyexpat import model
+from django.forms import ModelForm
+from . import models
+
+
+class AuthorForm(ModelForm):
+    class Meta:
+        model = models.Author
+        fields = ['name', 'title', 'birth_date']
+
+
+class BookForm(ModelForm):
+    class Meta:
+        model = models.Book
+        fields = ['name', 'authors']
